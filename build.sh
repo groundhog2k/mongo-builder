8666c2b6e7109104ac2547506e0fad1c96d9def1
Göran Pöhner <10630407+groundhog2k@users.noreply.github.com>
2023-03-05 17:31:33 +0100

    Fixed build output (#2)

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ python3 -m pip install -r etc/pip/compile-requirements.txt
 python3 buildscripts/scons.py install-servers --disable-warnings-as-errors CCFLAGS="-march=goldmont"
 cd build/install/bin
 for x in mongo*; do strip -s $x; done
+mkdir -p /buildoutput
+cp * /buildoutput
+cd /
