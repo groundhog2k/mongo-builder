0869feb1b35bde50e9fd37adb0e53af777e4bd6e
Göran Pöhner <10630407+groundhog2k@users.noreply.github.com>
2023-09-28 17:41:10 +0200

    Updated for MongoDB 6 and simplified build execution (#5)
    * Updated for MongoDB 6 and simplified build execution
    
    * Fixed sub-folder problem
    
    * Fixed path error

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM ubuntu:22.04
-RUN apt update && apt install git build-essential python3-dev libssl-dev python-dev-is-python3 libcurl4-openssl-dev libcurl4 liblzma-dev pip -y
+RUN apt update && apt install git build-essential python3-dev libssl-dev libcurl4-openssl-dev python-dev-is-python3 libcurl4 liblzma-dev pip docker.io -y
 COPY *.sh /
+ENTRYPOINT [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -3,11 +3,20 @@ git clone https://github.com/mongodb/mongo.git
 cd mongo
 version="$1"
 shift 1
+imagename="$1"
+shift 1
 git checkout tags/r$version -b v$version
 python3 -m pip install -r etc/pip/compile-requirements.txt
 python3 buildscripts/scons.py install-servers --disable-warnings-as-errors CCFLAGS="-march=goldmont" $@
 cd build/install/bin
 for x in mongo*; do strip -s $x; done
-mkdir -p /buildoutput
-cp * /buildoutput
-cd /
+mkdir -p /docker/buildoutput
+cp * /docker/buildoutput
+cd /docker
+echo "Building $imagename:$version"
+cat <<EOF | tee ./Dockerfile
+ARG MONGO_VERSION
+FROM mongo:$version
+COPY buildoutput/ /usr/bin
+EOF
+docker build -t ${imagename}:${version} .
